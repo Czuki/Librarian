3ef5042bbd965c9ca9e348e903f2001bf8ec1fd0
Czuki <bart3322@gmail.com>
2021-03-10 13:46:27 +0100

    bootstrap styling added

--- librarian_project/librarian/models.py
+++ librarian_project/librarian/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=32, blank=True)
+    last_name = models.CharField(max_length=32, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=128)
-    isbn = models.CharField(max_length=32)
+    name = models.CharField(max_length=128, blank=True)
+    isbn = models.CharField(max_length=32, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
--- librarian_project/librarian/views.py
+++ librarian_project/librarian/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
-
+from librarian.models import Author, Book
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'librarian/base.html')
+
+        context = {
+            'authors': Author.objects.all(),
+            'books': Book.objects.all(),
+                   }
+        return render(request, 'librarian/base.html', context)
 
 
 

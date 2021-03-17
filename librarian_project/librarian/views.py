faf67370dc000a35047383cd9d30ebcd5583735b
Czuki <bart3322@gmail.com>
2021-03-17 14:59:15 +0100

    login and registration

--- librarian_project/librarian/views.py
+++ librarian_project/librarian/views.py
@@ -3,6 +3,24 @@ from django.views import View
 from librarian.models import Author, Book
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'librarian/signup.html', {'form': form})
+
 
 class HomeView(View):
     def get(self, request):
--- librarian_project/librarian_project/urls.py
+++ librarian_project/librarian_project/urls.py
@@ -1,28 +1,16 @@
-"""librarian_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from librarian.views import HomeView, AuthorAdd, AuthorDelete, AuthorList, BookList
+from django.urls import path, include
+from librarian.views import HomeView, AuthorAdd, AuthorDelete, AuthorList, BookList, signup
+
 
 urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view()),
-    path('add-book/', HomeView.as_view()),
-    path('list-books/', BookList.as_view()),
-    path('add-author/', AuthorAdd.as_view()),
-    path('list-authors/', AuthorList.as_view()),
-    path('delete-author/<int:author_id>/', AuthorDelete.as_view()),
+    path('', HomeView.as_view(), name='home'),
+    path('add-book/', HomeView.as_view(), name='add-book'),
+    path('list-books/', BookList.as_view(), name='list-books'),
+    path('add-author/', AuthorAdd.as_view(), name='add-author'),
+    path('list-authors/', AuthorList.as_view(), name='list-authors'),
+    path('delete-author/<int:author_id>/', AuthorDelete.as_view(), name='delete-author'),
 ]
